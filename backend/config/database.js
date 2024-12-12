6bc189f2978a452a367e8d29ff1fe093627b97eb
Alfian Ramadani <Alfianr792@gmail.com>
2024-12-12 22:43:51 +0800

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,6 @@
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || '3306';
+const DB_NAME = process.env.DB_NAME || 'mydb';
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_URL = process.env.DB_URL ;
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+const app = express();
